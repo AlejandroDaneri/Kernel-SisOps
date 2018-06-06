c5d91820f631f926317be5537319acdb0d4645a0
Alejandro Daneri <ale_dan_94@hotmail.com>
2018-06-06 16:06:55 -0300

    comentario de como cerrar quemu

--- kern0-boot/exec.sh
+++ kern0-boot/exec.sh
@@ -8,3 +8,5 @@ ld -m elf_i386 -e0x100000  -Ttext 0x100000 kern0.o boot.o -o kern0
 
 # Lanzar
 qemu-system-i386 -serial mon:stdio -kernel kern0
+
+# Para cerrar QEMU hay que hacer Ctrl-A, soltar y después apretar la x (minúscula)
