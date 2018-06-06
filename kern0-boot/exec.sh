505b30948e5a228982459058d355738fc4a6e94e
Alejandro Daneri <ale_dan_94@hotmail.com>
2018-06-06 16:03:52 -0300

    ejemplo para levantar un kernel

---
+++ kern0-boot/exec.sh
@@ -0,0 +1,10 @@
+# Para levantar el kernel en el QEMU
+
+# Compilar C y ASM
+gcc -g -m32 -O1 -c kern0.c boot.S
+
+# Enlazar
+ld -m elf_i386 -e0x100000  -Ttext 0x100000 kern0.o boot.o -o kern0
+
+# Lanzar
+qemu-system-i386 -serial mon:stdio -kernel kern0
