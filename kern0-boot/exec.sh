36b2ece0d2447f8bf5df194d906eb4b740849be2
Alejandro Daneri <ale_dan_94@hotmail.com>
2018-06-06 16:11:21 -0300

    info para ver registros

--- kern0-boot/exec.sh
+++ kern0-boot/exec.sh
@@ -9,4 +9,8 @@ ld -m elf_i386 -e0x100000  -Ttext 0x100000 kern0.o boot.o -o kern0
 # Lanzar
 qemu-system-i386 -serial mon:stdio -kernel kern0
 
-# Para cerrar QEMU hay que hacer Ctrl-A, soltar y después apretar la x (minúscula)
+# Lo que sigue se tiene que hacer sobre la terminal que levanto QEMU.
+# Para cerrar QEMU hay que hacer Ctrl-A, soltar y después apretar la x (minúscula).
+# Para entrar al monitor: Ctrl-A y después c . Se puede ejecutar info registers
+# para ver todos los registros.
+
