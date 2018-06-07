f113f95796e497fb237c1718c3f5bc40023e2b37
Alejandro Daneri <ale_dan_94@hotmail.com>
2018-06-07 11:51:16 -0300

    termino cmdline

--- kern2/kern2.c
+++ kern2/kern2.c
@@ -1,5 +1,6 @@
 #include "decls.h"
 #include "multiboot.h"
+#include "lib/string.h"
 
 void kmain(const multiboot_info_t *mbi) {
     vga_write("kern2 loading.............", 8, 0x70);
@@ -7,8 +8,10 @@ void kmain(const multiboot_info_t *mbi) {
     if (mbi->flags) {
         char buf[256] = "cmdline: ";
         char *cmdline = (void *) mbi->cmdline;
-        // Aquí usar strlcat() para concatenar cmdline a buf.
-        // ...
+        strlcat(buf, cmdline, sizeof(buf));
         vga_write(buf, 9, 0x07);
     }
+    vga_write("A", 10, 0x57);
+    asm("hlt");
+    vga_write("B", 11, 0x60); //solo de prueba para ver si hace el halt
 }
