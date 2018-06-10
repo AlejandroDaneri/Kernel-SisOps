1400186db78c819304147c528ff3466f74f37ff3
Alejandro Daneri <ale_dan_94@hotmail.com>
2018-06-10 13:06:52 -0300

    mejoro makefile para que acepte todos los .S

--- kern2/parte 2/kern2.c
+++ kern2/parte 2/kern2.c
@@ -3,9 +3,10 @@
 
 void kmain(const multiboot_info_t *mbi) {
     vga_write("kern2 loading.............", 8, 0x70);
-
-    // A remplazar por una llamada a two_stacks(),
-    // definida en stacks.S.
+    if(mbi){}
+    two_stacks();
+    /*/ equivalente en C de two_stacks()
     vga_write("vga_write() from stack1", 12, 0x17);
     vga_write("vga_write() from stack2", 13, 0x90);
+    /*/
 }
