6d3545123cb905bc2ac6040bac6d832766054d15
Rodrigo Aparicio <rodriver999@hotmail.com>
2018-06-10 23:26:00 -0300

    kern2-exec

--- kern2/parte 2/kern2.c
+++ kern2/parte 2/kern2.c
@@ -15,47 +15,29 @@ void kmain(const multiboot_info_t *mbi) {
 static uint8_t stack1[USTACK_SIZE] __attribute__((aligned(4096)));
 static uint8_t stack2[USTACK_SIZE] __attribute__((aligned(4096)));
 
-
 void two_stacks_c() {
-    /*/
     // Inicializar al *tope* de cada pila.
-    uintptr_t *a = ...
-    uintptr_t *b = ...
-
+    uintptr_t *a = (uintptr_t*) stack1 + USTACK_SIZE;
+    uintptr_t *b = (uintptr_t*) stack2 + USTACK_SIZE;
+    
     // Preparar, en stack1, la llamada:
     vga_write("vga_write() from stack1", 15, 0x57);
-
-    // AYUDA 1: se puede usar alguna forma de pre- o post-
-    // incremento/decremento, según corresponda:
-    //
-    //     *(a++) = ...
-    //     *(++a) = ...
-    //     *(a--) = ...
-    //     *(--a) = ...
-
-    // AYUDA 2: para apuntar a la cadena con el mensaje,
-    // es suficiente con el siguiente cast:
-    //
-    //   ... a ... = (uintptr_t) "vga_write() from stack1";
+    
+    *(a--) = 0x58;
+    *(a--) = 20;
+    *(a) = (uintptr_t) "vga_write() from stack1";
 
     // Preparar, en s2, la llamada:
     vga_write("vga_write() from stack2", 16, 0xD0);
-
-    // AYUDA 3: para esta segunda llamada, usar esta forma de
-    // asignación alternativa:
+    
     b -= 3;
-    b[0] = ...
-    b[1] = ...
-    b[2] = ...
+    b[0] = (uintptr_t) "vga_write() from stack2";
+    b[1] = 21;
+    b[2] = 0xD0;
 
-    // Primera llamada usando task_exec().
-    task_exec((uintptr_t) vga_write, (uintptr_t) s1);
-
-    // Segunda llamada con ASM directo. Importante: no
-    // olvidar restaurar el valor de %esp al terminar, y
-    // compilar con: -fasm -fno-omit-frame-pointer.
-    asm("...; call *%1; ..."
-    : // no outputs
-    : "r"(s2), "r"(vga_write));
-    /*/
+    task_exec((uintptr_t) vga_write, (uintptr_t) a);
+        
+    asm("movl %0, %%esp; call %1; movl %%ebp, %%esp"
+        : 
+        : "r"(b), "r"(vga_write));    
 }
